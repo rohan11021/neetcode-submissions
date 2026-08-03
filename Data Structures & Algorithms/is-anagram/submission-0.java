86b7477884f11b5be0212c5325fb0ee78777e07d
rohan11021 <128038596+rohan11021@users.noreply.github.com>
2026-08-03 22:13:36 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> answer = new HashMap<>();
+
+        for(char character : s.toCharArray()){
+            answer.put(character,answer.getOrDefault(character, 0)+1);
+        }
+        for(char character : t.toCharArray()){
+            answer.put(character,answer.getOrDefault(character, 0)-1);
+        }
+        for (Integer value : answer.values()) {
+            if(value>0 || value<0)
+                return false;
+        }
+
+        return true;
+    }
+}
